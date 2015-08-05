3becea20a1ed8e8b465d62a8e2e87bd1a4e10060
iridion9 <iridion_us@yahoo.com>
2015-08-05 08:31:55 +0800

    application folder was added

--- application/libraries/Spyder_lib.php
+++ application/libraries/Spyder_lib.php
@@ -71,7 +71,25 @@ class Spyder_lib {
     public function ()
     {
     	
+    }	
+	
+	/*
+	Usage: Extract string between tags.
+    $string = '<h1>Hello World!</h1>';
+    $extract = stringExtract($string, '<h1>', '</h1>');
+    echo $extract;	// Echoes “Hello World!”
+	 */	
+    function stringExtract($item, $start, $end) {
+    	if (($startPos = stripos($item, $start)) === false) {	// If $start string is not found
+    		return false;	// Return false
+    	} else if (($endPos = stripos($item, $end)) === false) {	// If $end string is not found
+    		return false;	// Return false
+    	} else {
+    		$substrStart = $startPos + strlen($start);	// Assigning start position
+    		return substr($item, $substrStart, $endPos - $substrStart);	// Returning string between start and end positions
+    	}
     }				
+	
 	
 #####################################################################
 #	For Testing...
